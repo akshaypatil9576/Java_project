d7264a0bab5a8456d2bc2bc4bc70fd65961ce3dc
akshaypatil9576 <akkip9576@gmail.com>
2022-01-05 20:02:50 +0530

    addition program created

---
+++ src/com/akshay/demo1.java
@@ -0,0 +1,13 @@
+package com.akshay;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		int a =4356;
+		int c =1256;
+		double b=a-c;
+		System.out.println("Number of digits ="+b);
+
+	}
+
+}
